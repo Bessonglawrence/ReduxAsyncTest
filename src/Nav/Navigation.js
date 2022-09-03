bc30e29b4cd27b3337396ead10585a07ca27d012
bessonglawrence <orockbessonglawrence@gmail.com>
2022-09-03 21:28:12 +0100

    Created a RegisterScreen and an index page to carry all othe pages. Routed the pages in the navigation page.

--- src/Nav/Navigation.js
+++ src/Nav/Navigation.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import {Router, Stack, Scene} from 'react-native-router-flux';
+import {
+    LoginScreen,
+    OnboardingScreen,
+    RegisterScreen,
+} from '../screens'
 
 
 const Navigation = () => {
   return (
     <Router>
         <Stack key="root">
-          <Scene key="splash" component={Splash} hideNavBar={true} initial />
-          <Scene key="onboarding" component={Onboarding} hideNavBar={true} />
-          <Scene key="login" component={Login} hideNavBar={true} />
-          <Scene key="register" component={Register} hideNavBar={true} />
- 
+          <Scene key="splash" component={OnboardingScreen} hideNavBar={true} initial />
+          <Scene key="onboarding" component={LoginScreen} hideNavBar={true} />
+          <Scene key="register" component={RegisterScreen} hideNavBar={true} />
         </Stack>
       </Router>
   )
---
+++ src/screens/index.js
@@ -0,0 +1,9 @@
+import LoginScreen from "./LoginScreen/LoginScreen";
+import OnboardingScreen from "./OnboardingScreen/OnboardingScreen";
+import RegisterScreen from "./Register/RegisterScreen";
+
+export default{
+    LoginScreen,
+    OnboardingScreen,
+    RegisterScreen
+}
