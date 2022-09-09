135e0167e1819ce2c697b02f1b64de4437ab2732
bessonglawrence <orockbessonglawrence@gmail.com>
2022-09-09 22:30:02 +0100

    Created Home and Error folders with the respective files

---
+++ src/screens/Error/Error.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Error = () => {
+  return (
+    <View>
+      <Text style={{color: 'black'}}>This is the Error page</Text>
+    </View>
+  )
+}
+
+export default Error
+
+const styles = StyleSheet.create({})
