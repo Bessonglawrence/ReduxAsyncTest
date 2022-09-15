ae97f43845217ae58dca32757cf217ebc5e6f81c
bessonglawrence <orockbessonglawrence@gmail.com>
2022-09-15 16:13:06 +0100

    Added icons in the search bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import Icon  from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon  from 'react-native-vector-icons/MaterialIcons';
 
 const NavBar = ({navigation, title}) => {
   return (
     <View style={{flexDirection: 'row'}}>
-      <Icon name="arrow-left" color='grey' size={30}/>
-      <Text style={{color: 'grey'}}>{title}</Text>
+      <View>
+        <Icon name='arrow-left-alt' color='grey' size={30}/>
+        <Text style={{color: 'grey'}}>{title}</Text>
+      </View>
+      <Icon name='arrow-right-alt' color='lightblue' size={40}/>
     </View>
   )
 }
