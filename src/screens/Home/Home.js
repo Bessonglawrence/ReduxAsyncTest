d24bdc21edef7a6396191996b6c21bd062199d8c
bessonglawrence <orockbessonglawrence@gmail.com>
2022-09-18 15:52:53 +0100

    Wrote the addtodo function

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,17 +1,34 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native'
+import React,{useState} from 'react'
 import NavBar from '../../components/NavBar'
 import TodoInput from '../../components/TodoInput'
 import TodoItem from '../../components/TodoItem'
-import { Data } from '../../Data/Data'
+import {useSelector, useDispatch} from 'react-redux'
+import { AddTodo } from '../../redux/Actions/todoActions'
 
 
 
 const Home = () => {
 
+  const [todo, setTodo] = useState();
+
+  const handlAdd = () =>{
+    id = Math.floor(Math.random() * 1000)
+    if(todo.length > 4){
+      dispatch(AddTodo({
+        id: id,
+        title: todo
+      }))
+    }
+  }
+
   const renderItem = ({ item }) => (
     <TodoItem title={item.title} />
   );
+
+  const todos = useSelector( state => state)
+  const Data = todos.todos;
+  const dispatch = useDispatch();
 
   return (
     <View style={{flex: 1}}>
@@ -19,7 +36,7 @@ const Home = () => {
         title="Home"
         iconName='search'
        />
-      <TodoInput />
+      <TodoInput value={todo} onChangeText={setTodo} onPress={() => Alert.alert(`${todo}, was entered`)}/>
 
       <Text style={styles.headerText}>Todos</Text>
 
