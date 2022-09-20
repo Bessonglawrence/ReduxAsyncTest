1a35acb2fdba94f194d0d2eeee196f43ba34db1d
bessonglawrence <orockbessonglawrence@gmail.com>
2022-09-20 16:02:24 +0100

    Created animation for splash image

--- src/screens/SplashScreen/SplashScreen.js
+++ src/screens/SplashScreen/SplashScreen.js
@@ -2,17 +2,67 @@ import {
     StyleSheet, 
     Text, 
     View,
-    Image
+    Image,
+    Animated,
+    TouchableOpacity,
+    Easing
 } from 'react-native'
-import React from 'react'
+import React,{useEffect} from 'react'
 
 const SplashScreen = () => {
+
+ const handleAnimation = () => {
+    Animated.timing(animatedValue, {
+        toValue: 1,
+        duration: 1000,
+        easing: Easing.ease
+    }).start()
+}
+
+  const animatedValue = new Animated.Value(0);
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Image
+      <Animated.Image
         source={require('../../images/splash.png')} 
-        style={{width: 350, height: 350}}
+        style={{
+          width: 20, 
+          height: 20,
+          left: 50,
+          position: 'absolute',
+          transform: [
+            {
+              translateX: animatedValue.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, 120]
+              })
+            },
+            {
+                translateY: animatedValue.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [0, 25]
+                })
+            },
+            {
+                scaleX: animatedValue.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [1, 15]
+                })
+            },
+            {
+                scaleY: animatedValue.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [1, 12.5]
+                })
+            }
+          ]
+        }}
       />
+
+        <TouchableOpacity onPress={handleAnimation}>
+          <Text>
+              Transform Image
+          </Text>
+        </TouchableOpacity>
     </View>
   )
 }
