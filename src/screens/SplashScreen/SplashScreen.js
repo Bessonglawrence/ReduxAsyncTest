a14da65f7b0203587f936b8b4a45f965758815d4
bessonglawrence <orockbessonglawrence@gmail.com>
2022-09-21 15:58:20 +0100

    Added useEffect to make splash image animate when app loads

--- src/screens/SplashScreen/SplashScreen.js
+++ src/screens/SplashScreen/SplashScreen.js
@@ -15,54 +15,59 @@ const SplashScreen = () => {
     Animated.timing(animatedValue, {
         toValue: 1,
         duration: 1000,
-        easing: Easing.ease
+        easing: Easing.ease,
+        useNativeDriver: true
     }).start()
 }
 
+useEffect(() =>{
+  handleAnimation();
+},[]);
+
   const animatedValue = new Animated.Value(0);
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={{flex: 1, justifyContent: 'center',}}>
       <Animated.Image
         source={require('../../images/splash.png')} 
         style={{
           width: 20, 
           height: 20,
-          left: 50,
-          position: 'absolute',
+          alignSelf: 'center',
+          
           transform: [
             {
               translateX: animatedValue.interpolate({
                   inputRange: [0, 1],
-                  outputRange: [0, 120]
+                  outputRange: [0, 18]
               })
             },
             {
                 translateY: animatedValue.interpolate({
                     inputRange: [0, 1],
-                    outputRange: [0, 25]
+                    outputRange: [0, 18]
                 })
             },
             {
                 scaleX: animatedValue.interpolate({
                     inputRange: [0, 1],
-                    outputRange: [1, 15]
+                    outputRange: [1, 18]
                 })
             },
             {
                 scaleY: animatedValue.interpolate({
                     inputRange: [0, 1],
-                    outputRange: [1, 12.5]
+                    outputRange: [1, 18]
                 })
             }
           ]
         }}
       />
 
-        <TouchableOpacity onPress={handleAnimation}>
+        {/* <TouchableOpacity onPress={handleAnimation}>
           <Text>
               Transform Image
           </Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
     </View>
   )
 }
